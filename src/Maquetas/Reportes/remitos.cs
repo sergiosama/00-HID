af40cc7f9f50f3f23fae06838801d05591409da6
sergiosama <sergiosama.elturko@gmail.com>
2014-08-07 22:42:28 -0300

    Maquetas Reload

---
+++ src/Maquetas/Reportes/remitos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace Maquetas.Reportes
+{
+    public partial class remitos : DevExpress.XtraReports.UI.XtraReport
+    {
+        public remitos()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
