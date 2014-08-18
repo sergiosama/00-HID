c27f35af6ca0d47f550c8fe8d9b9089b7e5e5da5
ethedy <ethedy@gmail.com>
2014-08-18 04:52:00 -0300

    Agrego BarCode, elimino mdf de commit

--- src/Maquetas/frmMain.Designer.cs
+++ src/Maquetas/frmMain.Designer.cs
@@ -28,215 +28,117 @@
     /// </summary>
     private void InitializeComponent()
     {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
-            this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
-            this.bbiLogin = new DevExpress.XtraBars.BarButtonItem();
-            this.bbiAbout = new DevExpress.XtraBars.BarButtonItem();
-            this.bbiLogout = new DevExpress.XtraBars.BarButtonItem();
-            this.rbpgInicio = new DevExpress.XtraBars.Ribbon.RibbonPage();
-            this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
-            this.ribbonStatusBar1 = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
-            this.pnlContainer = new DevExpress.XtraEditors.XtraUserControl();
-            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
-            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
-            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
-            this.emptySpaceItem1 = new DevExpress.XtraLayout.EmptySpaceItem();
-            this.emptySpaceItem2 = new DevExpress.XtraLayout.EmptySpaceItem();
-            this.emptySpaceItem3 = new DevExpress.XtraLayout.EmptySpaceItem();
-            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
-            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
-            this.layoutControl1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // ribbonControl1
-            // 
-            this.ribbonControl1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.ribbonControl1.ExpandCollapseItem.Id = 0;
-            this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
-            this.ribbonControl1.ExpandCollapseItem,
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+      this.ribMain = new DevExpress.XtraBars.Ribbon.RibbonControl();
+      this.bbiLogin = new DevExpress.XtraBars.BarButtonItem();
+      this.bbiAbout = new DevExpress.XtraBars.BarButtonItem();
+      this.bbiLogout = new DevExpress.XtraBars.BarButtonItem();
+      this.rbpgInicio = new DevExpress.XtraBars.Ribbon.RibbonPage();
+      this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+      this.ribbonStatusBar1 = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
+      this.pnlContainer = new DevExpress.XtraEditors.XtraUserControl();
+      ((System.ComponentModel.ISupportInitialize)(this.ribMain)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // ribMain
+      // 
+      this.ribMain.Cursor = System.Windows.Forms.Cursors.Default;
+      this.ribMain.ExpandCollapseItem.Id = 0;
+      this.ribMain.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribMain.ExpandCollapseItem,
             this.bbiLogin,
             this.bbiAbout,
             this.bbiLogout});
-            this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
-            this.ribbonControl1.MaxItemId = 4;
-            this.ribbonControl1.Name = "ribbonControl1";
-            this.ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+      this.ribMain.Location = new System.Drawing.Point(0, 0);
+      this.ribMain.MaxItemId = 4;
+      this.ribMain.Name = "ribMain";
+      this.ribMain.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
             this.rbpgInicio});
-            this.ribbonControl1.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonControlStyle.Office2013;
-            this.ribbonControl1.Size = new System.Drawing.Size(1193, 144);
-            this.ribbonControl1.StatusBar = this.ribbonStatusBar1;
-            this.ribbonControl1.PageGroupCaptionButtonClick += new DevExpress.XtraBars.Ribbon.RibbonPageGroupEventHandler(this.GrupoClick);
-            this.ribbonControl1.SelectedPageChanged += new System.EventHandler(this.Ribbon_CambiarPagina);
-            // 
-            // bbiLogin
-            // 
-            this.bbiLogin.Caption = "Login";
-            this.bbiLogin.Id = 1;
-            this.bbiLogin.LargeGlyph = ((System.Drawing.Image)(resources.GetObject("bbiLogin.LargeGlyph")));
-            this.bbiLogin.Name = "bbiLogin";
-            // 
-            // bbiAbout
-            // 
-            this.bbiAbout.Caption = "Acerca de...";
-            this.bbiAbout.Glyph = ((System.Drawing.Image)(resources.GetObject("bbiAbout.Glyph")));
-            this.bbiAbout.Id = 2;
-            this.bbiAbout.LargeGlyph = ((System.Drawing.Image)(resources.GetObject("bbiAbout.LargeGlyph")));
-            this.bbiAbout.Name = "bbiAbout";
-            // 
-            // bbiLogout
-            // 
-            this.bbiLogout.Caption = "Logout";
-            this.bbiLogout.Enabled = false;
-            this.bbiLogout.Glyph = ((System.Drawing.Image)(resources.GetObject("bbiLogout.Glyph")));
-            this.bbiLogout.Id = 3;
-            this.bbiLogout.LargeGlyph = ((System.Drawing.Image)(resources.GetObject("bbiLogout.LargeGlyph")));
-            this.bbiLogout.Name = "bbiLogout";
-            // 
-            // rbpgInicio
-            // 
-            this.rbpgInicio.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+      this.ribMain.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonControlStyle.Office2013;
+      this.ribMain.Size = new System.Drawing.Size(1193, 144);
+      this.ribMain.StatusBar = this.ribbonStatusBar1;
+      this.ribMain.PageGroupCaptionButtonClick += new DevExpress.XtraBars.Ribbon.RibbonPageGroupEventHandler(this.GrupoClick);
+      this.ribMain.SelectedPageChanged += new System.EventHandler(this.Ribbon_CambiarPagina);
+      // 
+      // bbiLogin
+      // 
+      this.bbiLogin.Caption = "Login";
+      this.bbiLogin.Id = 1;
+      this.bbiLogin.LargeGlyph = ((System.Drawing.Image)(resources.GetObject("bbiLogin.LargeGlyph")));
+      this.bbiLogin.Name = "bbiLogin";
+      // 
+      // bbiAbout
+      // 
+      this.bbiAbout.Caption = "Acerca de...";
+      this.bbiAbout.Glyph = ((System.Drawing.Image)(resources.GetObject("bbiAbout.Glyph")));
+      this.bbiAbout.Id = 2;
+      this.bbiAbout.LargeGlyph = ((System.Drawing.Image)(resources.GetObject("bbiAbout.LargeGlyph")));
+      this.bbiAbout.Name = "bbiAbout";
+      // 
+      // bbiLogout
+      // 
+      this.bbiLogout.Caption = "Logout";
+      this.bbiLogout.Enabled = false;
+      this.bbiLogout.Glyph = ((System.Drawing.Image)(resources.GetObject("bbiLogout.Glyph")));
+      this.bbiLogout.Id = 3;
+      this.bbiLogout.LargeGlyph = ((System.Drawing.Image)(resources.GetObject("bbiLogout.LargeGlyph")));
+      this.bbiLogout.Name = "bbiLogout";
+      // 
+      // rbpgInicio
+      // 
+      this.rbpgInicio.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
             this.ribbonPageGroup1});
-            this.rbpgInicio.Name = "rbpgInicio";
-            this.rbpgInicio.Text = "INICIO";
-            // 
-            // ribbonPageGroup1
-            // 
-            this.ribbonPageGroup1.ItemLinks.Add(this.bbiLogin);
-            this.ribbonPageGroup1.ItemLinks.Add(this.bbiLogout);
-            this.ribbonPageGroup1.ItemLinks.Add(this.bbiAbout);
-            this.ribbonPageGroup1.Name = "ribbonPageGroup1";
-            this.ribbonPageGroup1.Text = "Acceso";
-            // 
-            // ribbonStatusBar1
-            // 
-            this.ribbonStatusBar1.Location = new System.Drawing.Point(0, 791);
-            this.ribbonStatusBar1.Name = "ribbonStatusBar1";
-            this.ribbonStatusBar1.Ribbon = this.ribbonControl1;
-            this.ribbonStatusBar1.Size = new System.Drawing.Size(1193, 31);
-            // 
-            // pnlContainer
-            // 
-            this.pnlContainer.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.pnlContainer.Appearance.Options.UseBackColor = true;
-            this.pnlContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlContainer.Location = new System.Drawing.Point(0, 144);
-            this.pnlContainer.Name = "pnlContainer";
-            this.pnlContainer.Size = new System.Drawing.Size(1193, 647);
-            this.pnlContainer.TabIndex = 3;
-            // 
-            // layoutControl1
-            // 
-            this.layoutControl1.Controls.Add(this.simpleButton1);
-            this.layoutControl1.Location = new System.Drawing.Point(190, 220);
-            this.layoutControl1.Name = "layoutControl1";
-            this.layoutControl1.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(901, 160, 250, 350);
-            this.layoutControl1.Root = this.layoutControlGroup1;
-            this.layoutControl1.Size = new System.Drawing.Size(479, 169);
-            this.layoutControl1.TabIndex = 6;
-            this.layoutControl1.Text = "layoutControl1";
-            // 
-            // simpleButton1
-            // 
-            this.simpleButton1.Location = new System.Drawing.Point(108, 135);
-            this.simpleButton1.Name = "simpleButton1";
-            this.simpleButton1.Size = new System.Drawing.Size(101, 22);
-            this.simpleButton1.StyleController = this.layoutControl1;
-            this.simpleButton1.TabIndex = 4;
-            this.simpleButton1.Text = "OK";
-            // 
-            // layoutControlGroup1
-            // 
-            this.layoutControlGroup1.CustomizationFormText = "layoutControlGroup1";
-            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
-            this.layoutControlGroup1.GroupBordersVisible = false;
-            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
-            this.emptySpaceItem1,
-            this.emptySpaceItem2,
-            this.emptySpaceItem3,
-            this.layoutControlItem1});
-            this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
-            this.layoutControlGroup1.Name = "layoutControlGroup1";
-            this.layoutControlGroup1.Size = new System.Drawing.Size(479, 169);
-            this.layoutControlGroup1.Text = "layoutControlGroup1";
-            this.layoutControlGroup1.TextVisible = false;
-            // 
-            // emptySpaceItem1
-            // 
-            this.emptySpaceItem1.AllowHotTrack = false;
-            this.emptySpaceItem1.CustomizationFormText = "emptySpaceItem1";
-            this.emptySpaceItem1.Location = new System.Drawing.Point(0, 0);
-            this.emptySpaceItem1.Name = "emptySpaceItem1";
-            this.emptySpaceItem1.Size = new System.Drawing.Size(459, 110);
-            this.emptySpaceItem1.Text = "emptySpaceItem1";
-            this.emptySpaceItem1.TextSize = new System.Drawing.Size(0, 0);
-            // 
-            // emptySpaceItem2
-            // 
-            this.emptySpaceItem2.AllowHotTrack = false;
-            this.emptySpaceItem2.CustomizationFormText = "emptySpaceItem2";
-            this.emptySpaceItem2.Location = new System.Drawing.Point(0, 110);
-            this.emptySpaceItem2.Name = "emptySpaceItem2";
-            this.emptySpaceItem2.Size = new System.Drawing.Size(459, 13);
-            this.emptySpaceItem2.Text = "emptySpaceItem2";
-            this.emptySpaceItem2.TextSize = new System.Drawing.Size(0, 0);
-            // 
-            // emptySpaceItem3
-            // 
-            this.emptySpaceItem3.AllowHotTrack = false;
-            this.emptySpaceItem3.CustomizationFormText = "emptySpaceItem3";
-            this.emptySpaceItem3.Location = new System.Drawing.Point(201, 123);
-            this.emptySpaceItem3.Name = "emptySpaceItem3";
-            this.emptySpaceItem3.Size = new System.Drawing.Size(258, 26);
-            this.emptySpaceItem3.Text = "emptySpaceItem3";
-            this.emptySpaceItem3.TextSize = new System.Drawing.Size(0, 0);
-            // 
-            // layoutControlItem1
-            // 
-            this.layoutControlItem1.Control = this.simpleButton1;
-            this.layoutControlItem1.CustomizationFormText = "layoutControlItem1";
-            this.layoutControlItem1.Location = new System.Drawing.Point(0, 123);
-            this.layoutControlItem1.Name = "layoutControlItem1";
-            this.layoutControlItem1.Size = new System.Drawing.Size(201, 26);
-            this.layoutControlItem1.Text = "layoutControlItem1";
-            this.layoutControlItem1.TextSize = new System.Drawing.Size(93, 13);
-            // 
-            // frmMain
-            // 
-            this.AllowFormGlass = DevExpress.Utils.DefaultBoolean.True;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1193, 822);
-            this.Controls.Add(this.layoutControl1);
-            this.Controls.Add(this.pnlContainer);
-            this.Controls.Add(this.ribbonStatusBar1);
-            this.Controls.Add(this.ribbonControl1);
-            this.Name = "frmMain";
-            this.Ribbon = this.ribbonControl1;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.StatusBar = this.ribbonStatusBar1;
-            this.Text = "HID Sistema - Pantalla Inicial";
-            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
-            this.layoutControl1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      this.rbpgInicio.Name = "rbpgInicio";
+      this.rbpgInicio.Text = "INICIO";
+      // 
+      // ribbonPageGroup1
+      // 
+      this.ribbonPageGroup1.ItemLinks.Add(this.bbiLogin);
+      this.ribbonPageGroup1.ItemLinks.Add(this.bbiLogout);
+      this.ribbonPageGroup1.ItemLinks.Add(this.bbiAbout);
+      this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+      this.ribbonPageGroup1.Text = "Acceso";
+      // 
+      // ribbonStatusBar1
+      // 
+      this.ribbonStatusBar1.Location = new System.Drawing.Point(0, 791);
+      this.ribbonStatusBar1.Name = "ribbonStatusBar1";
+      this.ribbonStatusBar1.Ribbon = this.ribMain;
+      this.ribbonStatusBar1.Size = new System.Drawing.Size(1193, 31);
+      // 
+      // pnlContainer
+      // 
+      this.pnlContainer.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+      this.pnlContainer.Appearance.Options.UseBackColor = true;
+      this.pnlContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.pnlContainer.Location = new System.Drawing.Point(0, 144);
+      this.pnlContainer.Name = "pnlContainer";
+      this.pnlContainer.Size = new System.Drawing.Size(1193, 647);
+      this.pnlContainer.TabIndex = 3;
+      // 
+      // frmMain
+      // 
+      this.AllowFormGlass = DevExpress.Utils.DefaultBoolean.True;
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1193, 822);
+      this.Controls.Add(this.pnlContainer);
+      this.Controls.Add(this.ribbonStatusBar1);
+      this.Controls.Add(this.ribMain);
+      this.Name = "frmMain";
+      this.Ribbon = this.ribMain;
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.StatusBar = this.ribbonStatusBar1;
+      this.Text = "HID Sistema - Pantalla Inicial";
+      ((System.ComponentModel.ISupportInitialize)(this.ribMain)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
 
-    private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+    private DevExpress.XtraBars.Ribbon.RibbonControl ribMain;
     private DevExpress.XtraBars.Ribbon.RibbonPage rbpgInicio;
     private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
     private DevExpress.XtraBars.BarButtonItem bbiLogin;
@@ -244,13 +146,6 @@
     private DevExpress.XtraBars.Ribbon.RibbonStatusBar ribbonStatusBar1;
     private DevExpress.XtraEditors.XtraUserControl pnlContainer;
     private DevExpress.XtraBars.BarButtonItem bbiLogout;
-    private DevExpress.XtraLayout.LayoutControl layoutControl1;
-    private DevExpress.XtraEditors.SimpleButton simpleButton1;
-    private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
-    private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem1;
-    private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem2;
-    private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem3;
-    private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
   }
 }
 
