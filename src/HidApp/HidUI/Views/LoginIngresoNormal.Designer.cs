f61478fccbd79c9c31d70e600bae81230476f70f
sergiosama <sergiosama.elturko@gmail.com>
2014-09-23 23:37:33 -0300

    DBcontext Modificado

--- src/HidApp/HidUI/Views/LoginIngresoNormal.Designer.cs
+++ src/HidApp/HidUI/Views/LoginIngresoNormal.Designer.cs
@@ -28,115 +28,115 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
-      this.btnLogin = new DevExpress.XtraEditors.SimpleButton();
-      this.txtUser = new DevExpress.XtraEditors.TextEdit();
-      this.txtPassword = new DevExpress.XtraEditors.TextEdit();
-      this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
-      this.btnCancelar = new DevExpress.XtraEditors.SimpleButton();
-      this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
-      this.errLogin = new DevExpress.XtraEditors.DXErrorProvider.DXErrorProvider(this.components);
-      ((System.ComponentModel.ISupportInitialize)(this.txtUser.Properties)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.txtPassword.Properties)).BeginInit();
-      ((System.ComponentModel.ISupportInitialize)(this.errLogin)).BeginInit();
-      this.SuspendLayout();
-      // 
-      // btnLogin
-      // 
-      this.btnLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-      this.btnLogin.Appearance.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
-      this.btnLogin.Appearance.Options.UseFont = true;
-      this.btnLogin.Location = new System.Drawing.Point(165, 110);
-      this.btnLogin.Margin = new System.Windows.Forms.Padding(10);
-      this.btnLogin.Name = "btnLogin";
-      this.btnLogin.Size = new System.Drawing.Size(120, 40);
-      this.btnLogin.TabIndex = 2;
-      this.btnLogin.Text = "Login";
-      this.btnLogin.Click += new System.EventHandler(this.LoginUser);
-      // 
-      // txtUser
-      // 
-      this.txtUser.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.components = new System.ComponentModel.Container();
+            this.btnLogin = new DevExpress.XtraEditors.SimpleButton();
+            this.txtUser = new DevExpress.XtraEditors.TextEdit();
+            this.txtPassword = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.btnCancelar = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.errLogin = new DevExpress.XtraEditors.DXErrorProvider.DXErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.txtUser.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPassword.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errLogin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLogin.Appearance.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnLogin.Appearance.Options.UseFont = true;
+            this.btnLogin.Location = new System.Drawing.Point(165, 110);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(10);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(120, 40);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.Click += new System.EventHandler(this.LoginUser);
+            // 
+            // txtUser
+            // 
+            this.txtUser.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-      this.txtUser.Location = new System.Drawing.Point(88, 10);
-      this.txtUser.Margin = new System.Windows.Forms.Padding(10);
-      this.txtUser.Name = "txtUser";
-      this.txtUser.Properties.Appearance.Font = new System.Drawing.Font("Segoe UI", 10F);
-      this.txtUser.Properties.Appearance.Options.UseFont = true;
-      this.txtUser.Size = new System.Drawing.Size(201, 24);
-      this.txtUser.TabIndex = 0;
-      // 
-      // txtPassword
-      // 
-      this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.txtUser.Location = new System.Drawing.Point(88, 10);
+            this.txtUser.Margin = new System.Windows.Forms.Padding(10);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Properties.Appearance.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtUser.Properties.Appearance.Options.UseFont = true;
+            this.txtUser.Size = new System.Drawing.Size(201, 24);
+            this.txtUser.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-      this.txtPassword.EditValue = "";
-      this.txtPassword.Location = new System.Drawing.Point(88, 54);
-      this.txtPassword.Margin = new System.Windows.Forms.Padding(10);
-      this.txtPassword.Name = "txtPassword";
-      this.txtPassword.Properties.Appearance.Font = new System.Drawing.Font("Wingdings", 12F);
-      this.txtPassword.Properties.Appearance.Options.UseFont = true;
-      this.txtPassword.Properties.PasswordChar = 'N';
-      this.txtPassword.Size = new System.Drawing.Size(201, 24);
-      this.txtPassword.TabIndex = 1;
-      // 
-      // labelControl1
-      // 
-      this.labelControl1.Appearance.Font = new System.Drawing.Font("Segoe UI", 10F);
-      this.labelControl1.Location = new System.Drawing.Point(10, 13);
-      this.labelControl1.Margin = new System.Windows.Forms.Padding(10);
-      this.labelControl1.Name = "labelControl1";
-      this.labelControl1.Size = new System.Drawing.Size(45, 17);
-      this.labelControl1.TabIndex = 1;
-      this.labelControl1.Text = "Usuario";
-      // 
-      // btnCancelar
-      // 
-      this.btnCancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-      this.btnCancelar.Appearance.Font = new System.Drawing.Font("Segoe UI", 10F);
-      this.btnCancelar.Appearance.Options.UseFont = true;
-      this.btnCancelar.Location = new System.Drawing.Point(10, 110);
-      this.btnCancelar.Margin = new System.Windows.Forms.Padding(10);
-      this.btnCancelar.Name = "btnCancelar";
-      this.btnCancelar.Size = new System.Drawing.Size(120, 40);
-      this.btnCancelar.TabIndex = 3;
-      this.btnCancelar.Text = "Cancelar";
-      this.btnCancelar.Click += new System.EventHandler(this.LoginCancelado);
-      // 
-      // labelControl2
-      // 
-      this.labelControl2.Appearance.Font = new System.Drawing.Font("Segoe UI", 10F);
-      this.labelControl2.Location = new System.Drawing.Point(10, 57);
-      this.labelControl2.Margin = new System.Windows.Forms.Padding(10);
-      this.labelControl2.Name = "labelControl2";
-      this.labelControl2.Size = new System.Drawing.Size(56, 17);
-      this.labelControl2.TabIndex = 7;
-      this.labelControl2.Text = "Password";
-      // 
-      // errLogin
-      // 
-      this.errLogin.ContainerControl = this;
-      // 
-      // LoginIngresoNormal
-      // 
-      this.Appearance.Font = new System.Drawing.Font("Segoe UI", 10F);
-      this.Appearance.Options.UseFont = true;
-      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.Controls.Add(this.txtUser);
-      this.Controls.Add(this.labelControl1);
-      this.Controls.Add(this.labelControl2);
-      this.Controls.Add(this.btnLogin);
-      this.Controls.Add(this.btnCancelar);
-      this.Controls.Add(this.txtPassword);
-      this.Margin = new System.Windows.Forms.Padding(10);
-      this.Name = "LoginIngresoNormal";
-      this.Size = new System.Drawing.Size(300, 162);
-      ((System.ComponentModel.ISupportInitialize)(this.txtUser.Properties)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.txtPassword.Properties)).EndInit();
-      ((System.ComponentModel.ISupportInitialize)(this.errLogin)).EndInit();
-      this.ResumeLayout(false);
-      this.PerformLayout();
+            this.txtPassword.EditValue = "";
+            this.txtPassword.Location = new System.Drawing.Point(88, 54);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(10);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Properties.Appearance.Font = new System.Drawing.Font("Wingdings", 12F);
+            this.txtPassword.Properties.Appearance.Options.UseFont = true;
+            this.txtPassword.Properties.PasswordChar = 'N';
+            this.txtPassword.Size = new System.Drawing.Size(201, 24);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.labelControl1.Location = new System.Drawing.Point(10, 13);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(10);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(45, 17);
+            this.labelControl1.TabIndex = 1;
+            this.labelControl1.Text = "Usuario";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCancelar.Appearance.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnCancelar.Appearance.Options.UseFont = true;
+            this.btnCancelar.Location = new System.Drawing.Point(10, 110);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(10);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(120, 40);
+            this.btnCancelar.TabIndex = 3;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.Click += new System.EventHandler(this.LoginCancelado);
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.labelControl2.Location = new System.Drawing.Point(10, 57);
+            this.labelControl2.Margin = new System.Windows.Forms.Padding(10);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(56, 17);
+            this.labelControl2.TabIndex = 7;
+            this.labelControl2.Text = "Password";
+            // 
+            // errLogin
+            // 
+            this.errLogin.ContainerControl = this;
+            // 
+            // LoginIngresoNormal
+            // 
+            this.Appearance.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.Appearance.Options.UseFont = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.txtPassword);
+            this.Margin = new System.Windows.Forms.Padding(10);
+            this.Name = "LoginIngresoNormal";
+            this.Size = new System.Drawing.Size(300, 162);
+            ((System.ComponentModel.ISupportInitialize)(this.txtUser.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPassword.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errLogin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
