ffcb4c6c6617491af6fdd3ac5a13ce0ad16b8526
sergiosama <sergiosama.elturko@gmail.com>
2014-10-27 21:11:23 -0300

    EF- Nuevo

---
+++ src/HidApp/Entidades/enTipoFactura.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Entidades
+{
+    public class enTipoFactura
+    {
+
+         public enTipoFactura()
+        {
+            this.TObraSocial = new HashSet<enTObraSocial>();
+        }
+    
+        public int IdTipoFactura { get; set; }
+        public string TipoFactura { get; set; }
+    
+        public virtual ICollection<enTObraSocial> TObraSocial { get; set; }
+    }
+}
---
+++ src/HidApp/HidUI/ViewModel/NavegableViewModel.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.Mvvm;
+using HidUI.Common;
+
+namespace HidUI.ViewModel
+{
+  //
+  //  Este es un VM que pertenece a una NavigableView, no deberia tener algo que lo distinga?
+  //  Si lo pensamos es similar o tiene bastantes similitudes con MainViewModel....
+  public class NavegableViewModel : ViewModelBase
+  {
+    private ViewType _selectedViewType;
+    private object _selectedView;
+    private readonly IViewLocator _locator;
+
+    public NavegableViewModel(IViewLocator locator)
+    {
+      //  ojo el IViewLocator deberia ser unico para toda la app!!!
+    }
+
+    public void SetDefaultView()
+    {
+      Helpers.MainForm.MainViewModel.NavigateTo(ViewType.StockInsumos);
+    }
+  }
+}
